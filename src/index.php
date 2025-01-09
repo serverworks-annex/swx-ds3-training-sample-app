524354c6940de29163a11e2065c0274be7897951
satoshi.kaneyasu <satoshi.kaneyasu@serverworks.co.jp>
2025-01-09 09:00:06 +0000

    fix: 静的解析でエラーになるコードを追加

--- src/index.php
+++ src/index.php
@@ -5,6 +5,11 @@
     </head>
     <body>
         <h1>xxxxxx作成</h1>
-        <?php echo gethostname(); ?>
+        <?php 
+        echo gethostname(); 
+        
+        // 下記の一行により静的解析（phpstan）でエラーになります。
+        echo $undefinedVariable;
+        ?>
     </body>
 </html>
